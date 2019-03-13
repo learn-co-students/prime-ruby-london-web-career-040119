c73f78eab1c1eeea6915b6d190d9fee237e5697f
rebeccaswebsite <rebecca.huseyin.14@ucl.ac.uk>
2019-03-13 11:30:13 +0000

    adding method to check if a number is prime

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(integer)
+  if integer <= 1 
+		return false
+	else
+		(2..integer-1).to_a.all? do |possible_factor|
+     	 integer % possible_factor != 0
+	end
+  end 
+end 
