9ebd4d3b89e5244c17baf3e4f412cde4ba19d01e
Francisco Costa <francisco@fcosta.pt>
2019-03-14 16:51:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+require 'pry'
+
+
+ def prime?(integer)
+   array = (3..integer - 1).to_a
+   prime_array = []
+
+   if integer <= 1
+     false
+   elsif integer == 2 || integer == 3
+     true
+   elsif integer % 2 == 0
+     false
+   else
+     array.each {|element| prime_array << integer % element}
+     if prime_array.include?(0)
+       false
+     else
+       true
+     end
+   end
+ end
