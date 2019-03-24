7180c21bcd2103e66a76fd1bfcff10d7fd6a636c
Leah Boyd <leahboyd08@gmail.com>
2019-03-24 20:53:43 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
 # Add  code here!
+# def prime?(array)
+#   array.select do |x|
+#     x.prime?
+#   end
+#   array
+# end
+
+# def prime?(num)
+#   if num == (condition)
+#     return true
+#   elsif(another condition)
+#     return false
+#   end
+# end
+
+    
+def prime?(number)
+  if number <= 1
+    return false
+  else
+    (2..number/2).none? { |n| number % n == 0 }
+  end
+end
+
+    
