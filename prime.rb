9a8a9761fde01c2bd6cb085dac9bd30bbed30910
Ay Has <ahashim983@yahoo.com>
2019-03-28 22:00:09 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
+def prime?(num)
+  arr = (2..100).to_a
+  if num < 2
+    return false
+  end
+  factors = arr.detect{|i| num % i == 0 && i != num}
+  if factors
+    return false
+  else
+    return true
+  end
+end
+
+
+
+
 # Add  code here!
